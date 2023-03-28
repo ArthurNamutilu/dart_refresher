658a1ac2315a9a0f9cb969fc591ae63ec638ef5c
Arthur namutilu <99438294+ArthurNamutilu@users.noreply.github.com>
2023-03-28 08:13:21 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,18 @@
+// function
+String getFullName(String firstName, String lastName) {
+  return '$firstName $lastName';
+}
+// if else
+void test(){
+  final name = "Developer";
+  if (name == 'Developer'){
+    print("Hi Software Engineer");
+  }
+  else 
+    print('hello there');
+}
+
+void main() {
+  test();
+  print(getFullName('Arthur', 'Namutilu'));
+}
