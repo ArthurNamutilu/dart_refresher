fabc05963ed7433aa35ffaaae9f00f7c8fd6f197
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-04-01 16:57:02 +0300

    Dart maps

---
+++ maps.dart
@@ -0,0 +1,18 @@
+// store data in form of key value pair same as python dictionary
+void main() {
+  // DECLARING MAP USING MAP LITERALS
+  // var map_name = {key1:value1, key2:value2....}
+  var car = {'name': 'Mercedes', 'year': '2021'};
+  print(car);
+  car['color'] = 'Black';
+  print(car);
+
+  // DECLARING MAP USING MAP CONTRUCTOR
+  // map_name[key] = value
+
+  var student = new Map();
+  student['name'] = 'Arthur';
+  student['age'] = 23;
+  student['gpa'] = 8.7;
+  print(student);
+}
