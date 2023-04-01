e465bc1b321e0fd7e9a8a238e39b2a873a2baa85
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-04-01 14:08:59 +0300

    Dart Sets

---
+++ set.dart
@@ -0,0 +1,9 @@
+void main() {
+  print('Initializing Set..');
+  var names = <String>{"Ricky", "Jiji"};
+  print(names);
+  names.add("Fred");
+  print(names);
+  names.addAll({"Arthur", "Namutilu"});
+  print(names);
+}
