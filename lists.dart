fca1bf39eee7da492ca1779c157b3dd417de436a
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-04-01 13:28:41 +0300

    Dart Lists

---
+++ lists.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Fixed Length List ???????
+
+  // Growable List
+  var names = ['Kenya', 'Tanzania', 'Rwanda'];
+  print(names);
+  names.add('Uganda'); // add() method
+  print(names);
+  names.addAll(['Ethiopia', 'Burundi', 'South Sudan']); // addAll() method
+  print(names);
+}
