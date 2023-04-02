b65a0456bc31e8256db5d709ccf654a7ec30e10e
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-04-02 16:13:27 +0300

    modifed null

--- nullSafety.dart
+++ nullSafety.dart
@@ -1,3 +1,19 @@
+void test(String? a, String? b, String? c) {
+  final String nonNull = a != null
+      ? a
+      : b != null
+          ? b
+          : c!;
+  print(nonNull);
+}
+
 void main() {
-  print("Hello");
+  print("Printing Non null");
+  test(null, null, 'okay');
+  final String? fname = null;
+  final String? mname = null;
+  final String? lname = 'Gg';
+  //   The ?? operator (null coalescing operator) returns the first non-null value among its operands from left to right
+  final String? nonNullValue = fname ?? mname ?? lname;
+  print(nonNullValue);
 }
