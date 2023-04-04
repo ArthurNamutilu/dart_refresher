24a666f25805513e94c567a725b860691ef08707
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-04-04 14:03:06 +0300

    Dart Functions

---
+++ functions.dart
@@ -0,0 +1,15 @@
+void main() {
+  int sum(int a, int b) {
+    int result = a + b;
+    return result;
+    //print('Result is $c');
+  }
+
+  //  function without argument -Function with No Parameter and Return Value
+  greetings() {
+    print("Hello there");
+  }
+
+  greetings();
+  print(sum(12, 78));
+}
