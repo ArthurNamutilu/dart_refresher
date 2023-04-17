33f6a5682cd1e779a7e6ae0a606e884aa9c1dc84
ArthurNamutilu <arthurggnamutilu@gmail.com>
2023-04-17 07:07:51 +0300

    inheritance in dart

--- class.dart
+++ class.dart
@@ -1,20 +1,38 @@
+void main() {
+  // object creation
+  Employee receptonist = Employee('Charity', 22);
+  receptonist.result();
+  Admin Gg = Admin('Arthur', 23);
+  Gg.publish();
+  Gg.result();
+}
+
 class Employee {
   String? name;
+  int? age;
 
-  // getter method
+  // Constructor
+  Employee(String name, int age) {
+    // can be nm & ag
+    this.name = name;
+    this.age = age;
+  }
+  /* getter method
   String? get emp_name {
     this.name = name;
-  }
+  }         */
 
   // function defination
   void result() {
-    print(name);
+    print('${name} logged in successfully');
   }
 }
 
-void main() {
-  // object creation
-  Employee receptonist = new Employee();
-  receptonist.name = "Charity";
-  receptonist.result();
+// inheritance
+class Admin extends Employee {
+  //constructor
+  Admin(String name, int age) : super(name, age);
+  void publish() {
+    print('$name is a super employee');
+  }
 }
